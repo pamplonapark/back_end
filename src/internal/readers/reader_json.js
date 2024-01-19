d66c933932dab45cff601622844e473e2fd430e3
Vera0011 <contact.vera0011@gmail.com>
2024-01-19 19:43:31 +0100

    Added abstract HTTP petition
    - Added HTTP request
    - Added example with XML query -> Underground parkings

---
+++ src/internal/readers/reader_json.js
@@ -0,0 +1,15 @@
+
+/*const requestHTTP = require("../functions/requests_https");
+
+EXAMPLE XML PETITION TO UNDERGROUND PARKINGS
+const x = async () => {
+  let data = await requestHTTP(
+    "parkings.pamplona.es",
+    "/parkings.xml",
+    "GET",
+    { accept: "text/xml" }
+  );
+  console.log(data);
+};
+
+x();*/
