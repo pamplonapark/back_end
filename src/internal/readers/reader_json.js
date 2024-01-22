d4fa76b5c2e8351deed523537f58bdab210f7d2a
Vera0011 <contact.vera0011@gmail.com>
2024-01-22 03:36:09 +0000

    Added HTTPS request

--- src/internal/readers/reader_json.js
+++ src/internal/readers/reader_json.js
@@ -1,15 +1,13 @@
+const requestHTTP = require("../functions/requests");
 
-/*const requestHTTP = require("../functions/requests_https");
-
-EXAMPLE XML PETITION TO UNDERGROUND PARKINGS
-const x = async () => {
-  let data = await requestHTTP(
-    "parkings.pamplona.es",
+const get_public_parking = async () => {
+  // Realizar petición
+  /*let data = await requestHTTP(
+    "https://sig.pamplona.es/ogc/ows?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=Pamplona%3ASEGU_Lin_EsRegZonas&INFO_FORMAT=application%2Fjson&CRS=EPSG%3A25830&STYLES=&WIDTH=522&HEIGHT=364&BBOX=606078.6754307873%2C4738107.512999999%2C614430.6754307873%2C4743931.512999999",
     "/parkings.xml",
-    "GET",
-    { accept: "text/xml" }
-  );
-  console.log(data);
+    "GET"
+  );*/
 };
 
-x();*/
+module.exports = get_public_parking;
+
