9ec8caeef733a8e9b385d6f778776f64c88edca8
Vera0011 <contact.vera0011@gmail.com>
2024-02-02 19:12:44 +0100

    Structured files

--- src/internal/readers/reader_json.js
+++ src/internal/readers/reader_json.js
@@ -1,13 +1,5 @@
 const requestHTTP = require("../functions/requests");
 
-const get_public_parking = async () => {
-  // Realizar petición
-  /*let data = await requestHTTP(
-    "https://sig.pamplona.es/ogc/ows?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=Pamplona%3ASEGU_Lin_EsRegZonas&INFO_FORMAT=application%2Fjson&CRS=EPSG%3A25830&STYLES=&WIDTH=522&HEIGHT=364&BBOX=606078.6754307873%2C4738107.512999999%2C614430.6754307873%2C4743931.512999999",
-    "/parkings.xml",
-    "GET"
-  );*/
-};
+const get_public_parking = async () => {};
 
 module.exports = get_public_parking;
-
--- src/internal/readers/reader_xml.js
+++ src/internal/readers/reader_xml.js
@@ -1,32 +1,15 @@
-// const requestHTTP = require('../functions/requests');
-const xml2js = require('xml2js');
-const fs = require('fs');
-const util = require('util');
-
-const writeFile = util.promisify(fs.writeFile);
+const xml2js = require("xml2js");
 
 const parseXMLData = async (data) => {
-    const parser = new xml2js.Parser({ attrkey: "ATTR" });
-    parser.parseString(data, async function(error, result) {
-        if (error) {
-            console.error(`Error parsing XML: ${error}`);
-        } else {
-            const jsonString = JSON.stringify(result, null, 1);
-            await writeFile('xml.json', jsonString);
-            console.log('JSON file has been written');
-        }
-    });
+  const parser = new xml2js.Parser({ attrkey: "ATTR" });
+
+  parser.parseString(data, async function (error, result) {
+    if (error) {
+      console.error(`Error parsing XML: ${error}`);
+    } else {
+      return JSON.stringify(result);
+    }
+  });
 };
 
-// const x = async () => {
-//     let data = await requestHTTP(
-//       "parkings.pamplona.es",
-//       "/parkings.xml",
-//       "GET",
-//       { accept: "text/xml" }
-//     );
-//     console.log(data);
-//     parseXMLData(data);
-// };
-
-// x();
+module.exports = parseXMLData;
