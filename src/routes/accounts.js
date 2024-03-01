df657e4a9340adb672ecbda60e2c56d8f5e0bffe
vicsurcor <vdsurcor@gmail.com>
2024-03-01 15:44:21 +0100

    Added getPersonalInfo / getFavorites

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -64,4 +64,86 @@ router.post("/register", async (req, res) => {
   //decode_bearer_token(req.headers.authorization.split(" ")[1]);
 });
 
+/**
+ * @swagger
+ * /getPersonalInfo:
+ *   get:
+ *     summary: Get personal information of a user
+ *     description: Retrieves personal information of a user based on username
+ *     parameters:
+ *       - in: query
+ *         name: username
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The username of the user
+ *     responses:
+ *       '200':
+ *         description: Personal information retrieved successfully
+ *       '404':
+ *         description: User not found
+ *       '500':
+ *         description: Internal server error
+ */
+router.get("/getPersonalInfo", async (req, res) => {
+  let body_parsed = JSON.parse(decrypt_aes(req.body.info, req.body.iv, req.body.authPath));
+
+  try {
+    // Fetch personal information of the user
+    const personalInfo = await executeQuery("SELECT * FROM users WHERE username = ?", [body_parsed.username]);
+
+    if (personalInfo.length === 0) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    // Return the personal information
+    return res.status(200).send(personalInfo);
+  } catch (error) {
+    logger.error("Error retrieving personal information:", error);
+    return res.status(500).send({ error: "Internal server error" });
+  }
+});
+
+/**
+ * @swagger
+ * /getUserFavorites:
+ *   get:
+ *     summary: Get user's favorite parkings
+ *     description: Retrieves the favorite parkings of a user based on user ID
+ *     parameters:
+ *       - in: query
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The ID of the user
+ *     responses:
+ *       '200':
+ *         description: User's favorite parkings retrieved successfully
+ *       '404':
+ *         description: User's favorite parkings not found
+ *       '500':
+ *         description: Internal server error
+ */
+router.get("/getUserFavorites", async (req, res) => {
+  let body_parsed = JSON.parse(decrypt_aes(req.body.info, req.body.iv, req.body.authPath));
+
+  try {
+    const userFavorites = await executeQuery("SELECT * FROM parkings WHERE id IN (SELECT id_parking FROM user_favorite WHERE id_user = ?)", [body_parsed.id]);
+
+    if (userFavorites.length === 0) {
+      return res.status(404).send({ error: "Favorites not found" });
+    }
+
+    // Return the user's favorites
+    return res.status(200).send(userFavorites);
+  } catch (error) {
+    logger.error("Error retrieving favorites:", error);
+    return res.status(500).send({ error: "Internal server error" });
+  }
+});
+
+
+
+
 module.exports = router;
